bd5a84579978a90b68555dd613137622868dd0a3
ugbeadie <ugbeadie3@gmail.com>
2022-03-13 11:43:55 +0100

    e-commerce

--- script.js
+++ script.js
@@ -1,10 +1,25 @@
 const toggleButton = document.getElementsByClassName("toggle-button")[0]
 const navUl = document.getElementsByClassName('nav-items')[0];
+const itemCount = document.getElementsByClassName('count')[0];
+const minus = document.getElementsByClassName('minus')[0];
+const plus = document.getElementsByClassName('plus')[0];
+
+let count = 0;
 
 toggleButton.addEventListener('click', () => {
     navUl.classList.toggle('active');
-    // navMenu.classList.toggle('active')
     document.body.classList.toggle('hidden')
-    // document.body.classList.toggle("black");
     document.body.classList.toggle("cross");
+});
+
+plus.addEventListener('click', () => {
+    itemCount.textContent = count+=1;
+})
+
+minus.addEventListener('click', () => {
+    if (count > 0) {
+        itemCount.textContent = count-=1;
+    }else {
+        count = 0;
+    }
 })
